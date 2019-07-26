54f59cbf531746b93d9235679989e37db47bafe1
olivierko <olivier@alldiksson.se>
2019-07-26 21:17:38 +0200

    - folder structure - csproj format

--- Widtop/Program.cs
+++ Widtop/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Widtop
 {
@@ -10,6 +6,8 @@ namespace Widtop
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Hello");
+            Console.ReadKey();
         }
     }
 }
